6ae1c4fdf95dab74da48df01e0ccf24f09d4863e
Rodrigo Amora <rodrigo.amora.freitas@gmail.com>
2024-04-11 03:38:36 -0300

    Criando a rota de receita

--- receitas/views.py
+++ receitas/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'receitas/index.html')
+    return render(request, 'index.html')
+
+def receita(request):
+    return render(request, 'receita.html')
